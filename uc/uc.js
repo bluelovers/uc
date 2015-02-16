ef8a125a59954d256a3d1f97e5bcdaedf6554894
bluelovers <codelovers@users.sourceforge.net>
2015-02-16 10:34:40 +0800

    jsbeautifier (Step: 2)
    Step: 2

--- uc/uc.js
+++ uc/uc.js
@@ -1,15 +1,33 @@
-({classes: Cc, interfaces: Ci, utils: Cu, results: Cr}) = Components
+(
+{
+	classes: Cc,
+	interfaces: Ci,
+	utils: Cu,
+	results: Cr
+}) = Components
 
-;(UC = {__proto__: Cu.import('resource://uc/uc.jsm', null).UC}).load(self)
+;
+(UC = {
+	__proto__: Cu.import('resource://uc/uc.jsm', null).UC
+}).load(self)
 
 UC.pget('extensions.uc.subwindows') &&
-document.documentElement.addEventListener(
-  'DOMContentLoaded', function UC_onContentLoad(ev){ UC.init(ev.target) }, true)
+	document.documentElement.addEventListener(
+		'DOMContentLoaded',
+		function UC_onContentLoad(ev)
+		{
+			UC.init(ev.target)
+		}, true)
 
-if(location.href === UC.URL_MAIN){
-  document.getElementById('menu_ToolsPopup').appendChild(UC.dom({
-    $: 'menuitem', label: 'uc', accesskey: 'u', oncommand: 'UC.options()',
-    class: 'menuitem-iconic',
-    image: 'resource://uc/chrome/icons/default/uc.ico',
-  }))
+if (location.href === UC.URL_MAIN)
+{
+	document.getElementById('menu_ToolsPopup').appendChild(UC.dom(
+	{
+		$: 'menuitem',
+		label: 'uc',
+		accesskey: 'u',
+		oncommand: 'UC.options()',
+		class: 'menuitem-iconic',
+		image: 'resource://uc/chrome/icons/default/uc.ico',
+	}))
 }
